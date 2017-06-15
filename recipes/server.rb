1805749d79bc7cf80c1d939450296044ad85699c
sindhureddyvr <sindhureddyvr@gmail.com>
2017-06-15 00:30:33 +0000

    Release version 0.2.0

--- recipes/server.rb
+++ recipes/server.rb
@@ -2,7 +2,9 @@ package 'httpd'
 
 file '/var/www/html/index.html' do
 action:create
-content '<h1>Hello,world!</h1>'
+content "<h1>Hello,world!</h1>
+         <h1>ipaddress: #{node['ipaddress']}</h1>
+         <h1>hostname : #{node['hostname']}<h1> "
 end
 
 service 'httpd' do
